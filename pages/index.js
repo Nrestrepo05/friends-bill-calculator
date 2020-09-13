e51fc8300d6a4e9b34d4ef3c2d878567298adcb0
nrestrepo05 <nrestrepo.r05@gmail.com>
2020-09-13 18:14:02 -0400

    Init the project 😁

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import react from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>Hello</div>
+      <style jsx global>
+        {`
+          body {
+            margin: 0;
+            padding: 0;
+            background: #ECF1F1;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default App;
